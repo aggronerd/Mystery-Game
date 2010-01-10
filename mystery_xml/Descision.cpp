4312474432898eab7bdb9ee58f61a6df063b3afb
no author <noone@nowhere>
2010-01-10 22:36:15 +0000

    Added xml schema and objects for desision structure.

---
+++ mystery_xml/Descision.cpp
@@ -0,0 +1,19 @@
+/*
+ * Descision.cpp
+ *
+ *  Created on: 31 Dec 2009
+ *      Author: greg
+ */
+
+#include "Descision.h"
+
+Descision::Descision()
+{
+  // TODO Auto-generated constructor stub
+
+}
+
+Descision::~Descision()
+{
+  // TODO Auto-generated destructor stub
+}
---
+++ mystery_xml/Option.cpp
@@ -0,0 +1,19 @@
+/*
+ * Option.cpp
+ *
+ *  Created on: 31 Dec 2009
+ *      Author: greg
+ */
+
+#include "Option.h"
+
+Option::Option()
+{
+  // TODO Auto-generated constructor stub
+
+}
+
+Option::~Option()
+{
+  // TODO Auto-generated destructor stub
+}
